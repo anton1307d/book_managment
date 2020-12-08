de1238a71308387343a3e1a835c5734fa178fe18
antonzubb1307d <zubilevych@convidera.com>
2020-12-08 18:51:56 +0200

    create book

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/books','App\Http\Controllers\Api\BookController@index');
+Route::post('/books/create','App\Http\Controllers\Api\BookController@create');
