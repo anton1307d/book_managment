7b1d0cf604ac6a4eeafc14e23e93ec41607b9001
antonzubb1307d <zubilevych@convidera.com>
2021-01-16 19:20:21 +0200

    fix

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -15,7 +15,7 @@ class ExampleTest extends TestCase
     {
         $this->withoutExceptionHandling();
 
-        $response = $this->get('/');
+        $response = $this->get('/api/books');
 
         $response->assertStatus(200);
     }
